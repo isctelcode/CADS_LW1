cd88baf515603568c6b775998f6f87204b18a6b9
isctel <stepulenko_kirill@mail.ru>
2025-09-10 20:30:00 +0300

    base_class

---
+++ CADS_LW1/Animal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADS_LW1
+{
+    internal class Animal
+    {
+        private string name;
+        private string gender;
+        private int age;
+
+        public string Name { get; set; }
+        public string Gender
+        {
+            get
+            { return gender; }
+            set
+            {
+                while (true)
+                {
+                    if (value != "Male" || value != "Female") { Console.Write("Wrong gender, try again: "); }
+                    else
+                    {
+                        gender = value;
+                        break;
+                    }
+                }
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                while (true)
+                {
+                    if (value < 0) { Console.Write("Wrong gender, try again: "); }
+                    else
+                    {
+                        age = value;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public Animal(string name, string gender, int age)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+        }
+
+        public Animal()
+        {
+            this.name = "Unknown";
+            this.gender = "Unknown";
+            this.age = 0;
+        }
+    }
+}
--- CADS_LW1/Program.cs
+++ CADS_LW1/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//иерархия классов
+//животное:
+//  птица
+//  млекопитающее:
+//      парнокопытное
+
 namespace CADS_LW1
 {
     internal class Program
